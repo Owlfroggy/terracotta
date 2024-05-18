417e59399a10524cfcb6d768701ce18dbcbfa5f3
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-05-18 16:18:01 -0400

    cli-ify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as Tokenizer from "./tokenizer"
 import * as ErrorHandler from "./errorHandler"  
 import * as Compiler from "./compiler"
+import { parseArgs } from "@pkgjs/parseargs"
+import * as path from "path"
+const ncp = require("copy-paste")
 
 export const DEBUG_MODE = {
     enableDebugFunctions: true,
@@ -13,44 +16,70 @@ export function print (...data: any[]) {
     console.log(...data,)
 }
 
-async function Main() { 
-    //tokenize
-    let FILE_PATH = "testscripts/general2.tc"
-    let script = await Bun.file(FILE_PATH).text()
-    let tokenResults: Tokenizer.TokenizerResults
+async function Main() {
+    const options = {
+        //the file to take in
+        file: { type: "string" },
+        
+        //if true, compile inputted script
+        compile: { type: "boolean" },
 
-    try {
-        tokenResults = Tokenizer.Tokenize(script)
-    } catch (e) {
-        ErrorHandler.PrintError(e,script)
-        return
-    }
+        //if true, copy the result to the clipboard
+        copy: { type: "boolean" },
 
-    // console.log("CODE LINES!!!")
-    // console.log(JSON.stringify(tokenResults!.Lines, null, "  "))
+        //"gzip": output gzipped df template json
+        //"json": output stringified df template json
+        //"dfgive": output a /dfgive command that gives the template
+        //default is "gzip"
+        cmode: { type: "string" },
+    };
 
-    let compiledResults: Compiler.CompileResults
-    try {
-        compiledResults = Compiler.Compile(tokenResults.Lines)
-    } catch (e) {
-        ErrorHandler.PrintError(e,script)
-        return
-    }
-
-    let jsonedResults: string = Compiler.JSONize(compiledResults.Code)
+    const { values, positionals } = parseArgs({ options });
     
-    let gzippedResults: string = Compiler.GZIP(jsonedResults)
+    //mode: --compile
+    if (values.compile) {
+        //tokenize
+        let script = await Bun.file(values.file).text()
+        let tokenResults: Tokenizer.TokenizerResults
 
-    console.log(gzippedResults)
+        try {
+            tokenResults = Tokenizer.Tokenize(script)
+        } catch (e) {
+            ErrorHandler.PrintError(e, script)
+            return
+        }
 
-    let itemstring = `light_blue_terracotta{PublicBukkitValues:{"hypercube:codetemplatedata":'{"author":"Terracotta","name":"Compiled Template","version":1,"code":"${gzippedResults}"}'},display:{Name:'{"text":"","extra":[{"text":"Compiled Code Template","italic":false,"color":"green"}]}'}}`
+        //compile
+        let compiledResults: Compiler.CompileResults
+        try {
+            compiledResults = Compiler.Compile(tokenResults.Lines)
+        } catch (e) {
+            ErrorHandler.PrintError(e,script)
+            return
+        }
 
-    //temporary thing to make importing to df easier
-    //only works on macos according to the stack overflow post i stole it from
-    var proc = require('child_process').spawn('pbcopy');
-    proc.stdin.write('/dfgive '+itemstring); proc.stdin.end();
+        var jsoned = Compiler.JSONize(compiledResults.Code)
+        var gzipped = Compiler.GZIP(jsoned)
 
-    console.log("Copied to clipboard")
+        var finalOutput: string = ""
+        switch (values.cmode) {
+            case undefined || "gzip":
+                finalOutput = gzipped
+                break
+            case "json":
+                finalOutput = jsoned
+                break
+            case "dfgive":
+                finalOutput = `/dfgive light_blue_terracotta{PublicBukkitValues:{"hypercube:codetemplatedata":'{"author":"Terracotta","name":"Compiled Template","version":1,"code":"${gzipped}"}'},display:{Name:'{"text":"","extra":[{"text":"${path.basename(values.file)}","italic":false,"color":"green"}]}'}}`
+                break
+        }
+
+        //copy to clipboard if you're into that
+        if (values.copy) {
+            ncp.copy(finalOutput)
+            print("Copied output to clipboard")
+        }
+    }   
 }
 
 await Main()
