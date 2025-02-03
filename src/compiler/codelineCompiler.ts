94a3bc744c7718add2d8899e5dca0bc9eb72d35d
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2025-02-03 18:12:49 -0500

    fix compiler logic to ensure temporary variables are only replaced in setter blocks  (omg copilot giving a sane commit description for once)

--- src/compiler/codelineCompiler.ts
+++ src/compiler/codelineCompiler.ts
@@ -2912,10 +2912,14 @@ export function CompileLines(lines: Array<Array<Token>>, environment: Compilatio
             if (returnType) {
                 //make sure variables match up
                 if (block.Arguments[0] instanceof VariableItem && nextBlock.Arguments[1] instanceof VariableItem && nextBlock.Arguments[0] instanceof VariableItem && block.Arguments[0].Name == nextBlock.Arguments[1].Name) {
-                    //replace temp var here with the var thats actually being set
-                    block.Arguments[0] = nextBlock.Arguments[0]
-                    //remove = block
-                    CodeLine.splice(codeIndex+1,1)
+                    //make sure this block is actually a setter and not a modifier
+                    let firstParameter = AD.DFActionMap["set_var"]?.[block.Action]?.Parameters[0]?.Groups[0][0]
+                    if ((block.Block == "call_func" && block.Arguments[0].IsTemporary) || (firstParameter?.DFType == "VARIABLE" && firstParameter?.Description == "Variable to set")) {
+                        //replace temp var here with the var thats actually being set
+                        block.Arguments[0] = nextBlock.Arguments[0]
+                        //remove = block
+                        CodeLine.splice(codeIndex+1,1)
+                    }
                 }
             }
 
