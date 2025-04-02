d6c2e402b7fe4467b76754d6ccf082a9594f7b93
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2025-04-02 19:01:43 -0400

    add dumpdiff update util script

---
+++ updateUtils/dumpdiff.ts
@@ -0,0 +1,51 @@
+// format: deno run updateUtils/dumptiff.ts path/to/old.json path/to/new.json
+import { parseArgs } from "node:util";
+import process from "node:process"
+import { readFile } from "node:fs/promises";
+import { Dict } from "../src/util/dict.ts";
+
+const { values, positionals } = parseArgs({args: process.argv, allowPositionals: true})
+
+if (positionals.length < 1) {
+    console.log("Missing path to old action dump")
+    process.exit(1)
+} else if (positionals.length < 2) {
+    console.log("Missing path to new action dump")
+    process.exit(1)
+}
+
+const oldDump = JSON.parse((await readFile(positionals[2]!)).toString())
+const newDump = JSON.parse((await readFile(positionals[3]!)).toString())
+
+// actions \\
+let seen: Dict<any> = {}
+
+oldDump.actions.forEach(action => {
+    if (!seen[action.codeblockName]) {
+        seen[action.codeblockName] = {}
+    }
+    seen[action.codeblockName][action.name] = true
+});
+
+console.log("== NEW ACTIONS ==")
+newDump.actions.forEach(action => {
+    if (!seen[action.codeblockName][action.name]) {
+        console.log(`${action.codeblockName}:${action.name}`)
+    }
+})
+
+// values \\
+seen = {}
+
+oldDump.gameValues.forEach(value => {
+    seen[value.icon.name] = true
+});
+
+console.log("== NEW GAME VALUES ==")
+newDump.gameValues.forEach(value => {
+    if (!seen[value.icon.name]) {
+        console.log(`${value.icon.name}`)
+    }
+})
+
+export {}
