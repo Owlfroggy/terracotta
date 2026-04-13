644b88f043e955e7f59d0b66afc390dd578bde08
Owlfroggy <140653655+Owlfroggy@users.noreply.github.com>
2026-04-12 20:55:34 -0400

    single-value vector constructor

--- src/compiler/namespace/constructors.ts
+++ src/compiler/namespace/constructors.ts
@@ -1,21 +1,39 @@
 import { DFCodeblockName } from "../../df/constants.ts";
 import { Type } from "../../typeProcessor/type.ts";
 import { ActionBlock } from "../codeBlock.ts";
-import { MissingValue, NumberValue, TangibleValue, VectorValue } from "../codeValue.ts";
+import { CodeValue, MissingValue, NumberValue, TangibleValue, VectorValue } from "../codeValue.ts";
 import { DefinitionType, FunctionDefinition } from "./definition.ts";
 
 export const VEC_CONSTRUCTOR: FunctionDefinition = {
     definitionType: DefinitionType.FUNCTION,
     name: "vec",
     returnType: Type.vec,
-    signatures: [{params: [
-        {name: "x", type: Type.num, optional: false, plural: false},
-        {name: "y", type: Type.num, optional: false, plural: false},
-        {name: "z", type: Type.num, optional: false, plural: false},
-    ]}],
+    signatures: [
+        {
+            params: [
+                {name: "x", type: Type.num, optional: false, plural: false},
+                {name: "y", type: Type.num, optional: false, plural: false},
+                {name: "z", type: Type.num, optional: false, plural: false},
+            ]
+        },
+        {
+            params: [
+                {name: "value", type: Type.num, optional: false, plural: false, description: "Creates a vector with its x, y, and z components all set to this value"},
+            ]
+        },
+    ],
     // TODO: proper arg validation
     compile(args, namedArgs, ctx, callNode) {
-        if (args.length != 3) {
+        let x: CodeValue, y: CodeValue, z: CodeValue;
+        if (args.length == 3) {
+            x = args[0];
+            y = args[1];
+            z = args[2];
+        } else if (args.length == 1) {
+            x = args[0];
+            y = args[0];
+            z = args[0];
+        } else {
             ctx.reportError(
                 callNode.args,
                 "(temporary error) not enough arguments"
@@ -24,8 +42,8 @@ export const VEC_CONSTRUCTOR: FunctionDefinition = {
         }
 
         // constant vector
-        if (args[0] instanceof NumberValue && args[1] instanceof NumberValue && args[2] instanceof NumberValue) {
-            return [new VectorValue(args[0].value, args[1].value, args[2].value), []];
+        if (x instanceof NumberValue && y instanceof NumberValue && z instanceof NumberValue) {
+            return [new VectorValue(x.value, y.value, z.value), []];
         }
         // non-constant vector
         else {
@@ -33,7 +51,7 @@ export const VEC_CONSTRUCTOR: FunctionDefinition = {
             return [tempVar, [
                 new ActionBlock(DFCodeblockName.SET_VARIABLE,{
                     action: "Vector",
-                    args: [tempVar, ...args] as TangibleValue[], // todo: this is awful
+                    args: [tempVar, x, y, z] as TangibleValue[], // todo: this is awful and will likely cause crashes
                     astNode: callNode,
                 })
             ]];
