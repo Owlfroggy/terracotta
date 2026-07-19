7b2904e468c1eb482a605a103ab7b941e6c1de05
Owlfroggy <140653655+Owlfroggy@users.noreply.github.com>
2026-07-19 17:14:50 -0400

    update version

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import { DFRank } from "./df/constants.ts";
 import { PCodeParser } from "./pcode/pcodeParser.ts";
 import { normalizeLineEndings } from "./util/utils.ts";
 
-export const VERSION = "1.0.0-beta.3";
+export const VERSION = "1.0.0-beta.4";
 
 registerBuiltinNamespaces()
 
